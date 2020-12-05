b641f0572eb1a44b7945080979a425c439b91648
Michael Berry <michael.berry@sainsburys.co.uk>
2020-12-05 13:11:45 +0000

    Day5 improvements

--- src/main/java/com/github/berry120/adventofcode_2020/Day5.java
+++ src/main/java/com/github/berry120/adventofcode_2020/Day5.java
@@ -1,17 +1,15 @@
 package com.github.berry120.adventofcode_2020;
 
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class Day5 {
 
-  private final String[] lines;
-
-  public Day5(String input) {
-    lines = input.split("\n");
-  }
+  private final String input;
 
   private int seatNum(String line) {
     return Integer.parseInt(
@@ -20,24 +18,20 @@ public class Day5 {
   }
 
   public int part1() {
-    return Arrays.stream(lines)
+    return input.lines()
         .mapToInt(this::seatNum)
         .max()
         .orElseThrow();
   }
 
   public long part2() {
-    Set<Integer> seats = IntStream
-        .rangeClosed(Arrays.stream(lines)
-            .mapToInt(this::seatNum)
-            .min()
-            .orElseThrow(), part1())
-        .boxed()
-        .collect(Collectors.toSet());
+    Set<Integer> allSeats = input.lines().map(this::seatNum).collect(Collectors.toSet());
 
-    seats.removeAll(Arrays.stream(lines).map(this::seatNum).collect(Collectors.toSet()));
-
-    return seats.iterator().next();
+    return IntStream
+        .rangeClosed(allSeats.stream().min(Comparator.naturalOrder()).orElseThrow(), part1())
+        .filter(s -> !allSeats.contains(s))
+        .findAny()
+        .orElseThrow();
   }
 
 }
