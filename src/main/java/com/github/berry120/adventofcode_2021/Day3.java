4fa83cd25e7ff1a64f825253f512a37877a51193
Michael Berry <michael.berry@sainsburys.co.uk>
2021-12-03 12:20:45 +0000

    Improvements

--- src/main/java/com/github/berry120/adventofcode_2021/Day3.java
+++ src/main/java/com/github/berry120/adventofcode_2021/Day3.java
@@ -1,24 +1,44 @@
 package com.github.berry120.adventofcode_2021;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.stream.IntStream;
 
 public class Day3 {
 
-  private List<String> lines;
+  private final List<Integer> nums;
+  private final int length;
 
   public Day3(String input) {
-    lines = Arrays.asList(input.split("\n"));
+    nums = Arrays.stream(input.split("\n")).map(s -> Integer.parseInt(s, 2)).toList();
+    length = Integer.toBinaryString(Collections.max(nums)).length();
   }
 
-  private int mostCommon(List<String> lines, BiPredicate<Long, Long> p) {
-    return IntStream.range(0, lines.get(0).length())
-        .map(idx -> p.test(lines.stream().filter(s -> s.charAt(idx) == '1').count(),
-            lines.stream().filter(s -> s.charAt(idx) == '0').count()) ? 1 : 0)
-        .reduce(this::appendBinary)
-        .orElseThrow();
+  public int part1() {
+    return mostCommon(nums, (o, z) -> o >= z) * mostCommon(nums, (o, z) -> o < z);
+  }
+
+  public int part2() {
+    return ratings((o, z) -> o >= z) * ratings((o, z) -> o < z);
+  }
+
+  private int mostCommon(List<Integer> lines, BiPredicate<Long, Long> p) {
+    return IntStream.range(0, length).map(idx ->
+            p.test(lines.stream().filter(s -> bitAt(s, idx) == 1).count(),
+                lines.stream().filter(s -> bitAt(s, idx) == 0).count()) ? 1 : 0)
+        .reduce(this::appendBinary).orElseThrow();
+  }
+
+  public int ratings(BiPredicate<Long, Long> predicate) {
+    List<Integer> remaining = nums;
+    for (int i = 0; i < length && remaining.size() > 1; i++) {
+      final int idx = i;
+      int bit = bitAt(mostCommon(remaining, predicate), idx);
+      remaining = remaining.stream().filter(val -> bitAt(val, idx) == bit).toList();
+    }
+    return remaining.get(0);
   }
 
   private int appendBinary(int a, int b) {
@@ -29,29 +49,7 @@ public class Day3 {
     return r1;
   }
 
-  public int part1() {
-    return mostCommon(lines, (o, z) -> o >= z) * mostCommon(lines, (o, z) -> o < z);
+  private int bitAt(int val, int i) {
+    return (val >> length - 1 - i) & 1;
   }
-
-  public int part2() {
-    return part2((o, z) -> o >= z) * part2((o, z) -> o < z);
-  }
-
-  public int part2(BiPredicate<Long, Long> predicate) {
-    List<String> remaining = lines;
-    for (int i = 0; i < remaining.get(0).length(); i++) {
-      final int idx = i;
-
-      char c = ((mostCommon(remaining, predicate)
-                 >> remaining.get(0).length() - 1 - i)
-                & 1) == 1 ? '1' : '0';
-
-      remaining = remaining.stream().filter(s -> s.charAt(idx) == c).toList();
-      if (remaining.size() == 1) {
-        return Integer.parseInt(remaining.get(0), 2);
-      }
-    }
-    throw new RuntimeException();
-  }
-
 }
