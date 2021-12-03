e8aaa72913551ff3957f31e87d15959b664eda57
Michael Berry <michael.berry@sainsburys.co.uk>
2021-12-03 11:44:10 +0000

    Improvements

--- src/main/java/com/github/berry120/adventofcode_2021/Day3.java
+++ src/main/java/com/github/berry120/adventofcode_2021/Day3.java
@@ -2,60 +2,49 @@ package com.github.berry120.adventofcode_2021;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
-import lombok.AllArgsConstructor;
-import lombok.With;
+import java.util.function.BiPredicate;
+import java.util.stream.IntStream;
 
-@AllArgsConstructor
 public class Day3 {
 
-  private String input;
+  private List<String> lines;
 
-  public static int getBit(int num, int position) {
-    return (num >> position) & 1;
+  public Day3(String input) {
+    lines = Arrays.asList(input.split("\n"));
   }
 
-  int mostCommon(String input, Predicate<Tracker> p) {
-    String output = "";
-    String[] lines = input.split("\n");
-    int lineLength = lines[0].length();
-    for (int i = 0; i < lineLength; i++) {
-      final int idx = i;
-      int num1s = (int) Arrays.stream(lines).filter(s -> s.charAt(idx) == '1').count();
-      int num0s = lines.length - num1s;
-      Tracker t = new Tracker(num1s, num0s);
-      if (p.test(t)) {
-        output += "1";
-      } else {
-        output += "0";
-      }
+  private int mostCommon(List<String> lines, BiPredicate<Long, Long> p) {
+    return IntStream.range(0, lines.get(0).length())
+        .map(idx -> p.test(lines.stream().filter(s -> s.charAt(idx) == '1').count(),
+            lines.stream().filter(s -> s.charAt(idx) == '0').count()) ? 1 : 0)
+        .reduce(this::appendBinary)
+        .orElseThrow();
+  }
+
+  private int appendBinary(int a, int b) {
+    int r1 = a << -Integer.numberOfLeadingZeros(b) | b;
+    if (b == 0) {
+      r1 <<= 1;
     }
-    return Integer.parseInt(output, 2);
+    return r1;
   }
 
   public int part1() {
-    return mostCommon(input, t -> t.ones() >= t.zeros()) * mostCommon(input,
-        t -> t.ones() < t.zeros());
+    return mostCommon(lines, (o, z) -> o >= z) * mostCommon(lines, (o, z) -> o < z);
   }
 
   public int part2() {
-    return part2(t -> t.ones() >= t.zeros()) * part2(t -> t.ones() < t.zeros());
+    return part2((o, z) -> o >= z) * part2((o, z) -> o < z);
   }
 
-  public int part2(Predicate<Tracker> predicate) {
-    List<String> remaining = Arrays.asList(input.split("\n"));
+  public int part2(BiPredicate<Long, Long> predicate) {
+    List<String> remaining = lines;
     for (int i = 0; i < remaining.get(0).length(); i++) {
       final int idx = i;
 
-      int result = mostCommon(String.join("\n", remaining), predicate);
-
-      int x = getBit(result, remaining.get(0).length() - 1 - i);
-      char c;
-      if (x == 1) {
-        c = '1';
-      } else {
-        c = '0';
-      }
+      char c = ((mostCommon(remaining, predicate)
+                 >> remaining.get(0).length() - 1 - i)
+                & 1) == 1 ? '1' : '0';
 
       remaining = remaining.stream().filter(s -> s.charAt(idx) == c).toList();
       if (remaining.size() == 1) {
@@ -65,8 +54,4 @@ public class Day3 {
     throw new RuntimeException();
   }
 
-  @With
-  record Tracker(int ones, int zeros) {
-
-  }
 }
